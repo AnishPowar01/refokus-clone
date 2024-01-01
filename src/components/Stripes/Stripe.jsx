dc4f14a8d8d5e53f2a6723d354dd647221d2dafc
AnishPowar01 <powaranish74@gmail.com>
2024-01-01 20:25:34 +0530

    Added Stripe Component

---
+++ src/components/Stripes/Stripe.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Stripe = ({val}) => {
+    
+  return (
+    <div className='w-[16.66%] px-4 py-5 border-t-[1px] border-b-[1px] border-r-[1px]  border-zinc-600 flex justify-between items-center'>
+         <img src={val.url} alt="" />
+        <span className='font-semibold'>{val.number}</span>
+
+    </div>
+  )
+}
+
+export default Stripe
